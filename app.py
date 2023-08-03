73b0103a2fcbcab5eb5bbad806f579c92a096a26
cappuch <139052997+cappuch@users.noreply.github.com>
2023-08-03 02:36:53 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,13 +6,7 @@
 
 # Updated to account for UI changes from https://github.com/rkfg/audiocraft/blob/long/app.py
 # also released under the MIT license.
-import argparse
-import base64
-from flask import Flask, request, jsonify
-# ... (other imports)
 
-# Initialize Flask app
-app = Flask(__name__)
 import argparse
 from concurrent.futures import ProcessPoolExecutor
 import os
@@ -242,18 +236,6 @@ def ui_full(launch_kwargs):
 
 def ui_batched(launch_kwargs):
     with gr.Blocks() as demo:
-        gr.Markdown(
-            """
-            # MusicGen
-
-            This is the demo for [MusicGen](https://github.com/facebookresearch/audiocraft), a simple and controllable model for music generation
-            presented at: ["Simple and Controllable Music Generation"](https://huggingface.co/papers/2306.05284).
-            <br/>
-            <a href="https://huggingface.co/spaces/facebook/MusicGen?duplicate=true" style="display: inline-block;margin-top: .5em;margin-right: .25em;" target="_blank">
-            <img style="margin-bottom: 0em;display: inline;margin-top: -.25em;" src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
-            for longer sequences, more control and no queue.</p>
-            """
-        )
         with gr.Row():
             with gr.Column():
                 with gr.Row():
@@ -263,63 +245,54 @@ def ui_batched(launch_kwargs):
                     submit = gr.Button("Generate")
             with gr.Column():
                 output = gr.Video(label="Generated Music")
-        submit.click(predict_batched, inputs=[text, melody], outputs=[output], batch=True, max_batch_size=MAX_BATCH_SIZE)
-        gr.Examples(
-            fn=predict_batched,
-            examples=[
-                [
-                    "An 80s driving pop song with heavy drums and synth pads in the background",
-                    "./assets/bach.mp3",
-                ],
-                [
-                    "A cheerful country song with acoustic guitars",
-                    "./assets/bolero_ravel.mp3",
-                ],
-                [
-                    "90s rock song with electric guitar and heavy drums",
-                    None,
-                ],
-                [
-                    "a light and cheerly EDM track, with syncopated drums, aery pads, and strong emotions bpm: 130",
-                    "./assets/bach.mp3",
-                ],
-                [
-                    "lofi slow bpm electro chill with organic samples",
-                    None,
-                ],
-            ],
-            inputs=[text, melody],
-            outputs=[output]
-        )
-        gr.Markdown("""
-        ### More details
-
-        The model will generate 12 seconds of audio based on the description you provided.
-        You can optionaly provide a reference audio from which a broad melody will be extracted.
-        The model will then try to follow both the description and melody provided.
-        All samples are generated with the `melody` model.
-
-        You can also use your own GPU or a Google Colab by following the instructions on our repo.
-
-        See [github.com/facebookresearch/audiocraft](https://github.com/facebookresearch/audiocraft)
-        for more details.
-        """)
-
+        submit.click(predict_batched, inputs=[request.text], outputs=[output], batch=True, max_batch_size=MAX_BATCH_SIZE)
+        print(output)
         demo.queue(max_size=8 * 4).launch(**launch_kwargs)
 
 
-def submit():
-    data = request.json
-    text = data['text']
-    melody = data.get('melody', None)  # Optional melody input
-    # ... (other data extraction)
-    
-    # Process the text and melody inputs
-    outputs = predict_full(model, text, melody, duration, topk, topp, temperature, cfg_coef)
-    
-    # Convert the output .wav file to base64 encoded string
-    with open(outputs[0], 'rb') as wav_file:
-        wav_data = wav_file.read()
-        base64_wav = base64.b64encode(wav_data).decode('utf-8')
-    
-    return jsonify({'base64_wav': base64_wav})
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--listen',
+        type=str,
+        default='0.0.0.0' if 'SPACE_ID' in os.environ else '127.0.0.1',
+        help='IP to listen on for connections to Gradio',
+    )
+    parser.add_argument(
+        '--username', type=str, default='', help='Username for authentication'
+    )
+    parser.add_argument(
+        '--password', type=str, default='', help='Password for authentication'
+    )
+    parser.add_argument(
+        '--server_port',
+        type=int,
+        default=0,
+        help='Port to run the server listener on',
+    )
+    parser.add_argument(
+        '--inbrowser', action='store_true', help='Open in browser'
+    )
+    parser.add_argument(
+        '--share', action='store_true', help='Share the gradio UI'
+    )
+
+    args = parser.parse_args()
+
+    launch_kwargs = {}
+    launch_kwargs['server_name'] = args.listen
+
+    if args.username and args.password:
+        launch_kwargs['auth'] = (args.username, args.password)
+    if args.server_port:
+        launch_kwargs['server_port'] = args.server_port
+    if args.inbrowser:
+        launch_kwargs['inbrowser'] = args.inbrowser
+    if args.share:
+        launch_kwargs['share'] = args.share
+
+    # Show the interface
+    if IS_BATCHED:
+        ui_batched(launch_kwargs)
+    else:
+        ui_full(launch_kwargs)
